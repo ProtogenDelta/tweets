fb203040e6bef939ed148e0ca1254b5543741dac
Delta <47292121+ProtogenDelta@users.noreply.github.com>
2022-11-09 00:38:38 +1000

    Created some aliases (see `files/aliases.sh`) for ease of use.
    This is actually pretty cool...
    And 100% not what git was designed for, but meh.

---
+++ files/aliases.sh
@@ -0,0 +1,6 @@
+# git-twitter commands
+alias gtp='git commit --allow-empty' # post
+alias gts='git push origin main' # sync/send (push)
+alias gtr='git fetch' # refresh (fetch)
+alias gtl='git log --format="format:%Cred%cd %Cblue%h %Cgreen%cn%Creset: %s" --all --date=iso-local' # log
+alias gtv='git show' # view
