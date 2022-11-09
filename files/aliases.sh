21971ab1dacfe168954a2659078576beefb6d47d
Delta <47292121+ProtogenDelta@users.noreply.github.com>
2022-11-09 16:35:47 +1000

    Updated aliases... (ref 85a9059)
    Turns out git already provides a ton of short aliases that do what I want.
    No sense in having duplicates, so I decided to cut down the aliases file
    to just the unique stuff.

--- files/aliases.sh
+++ files/aliases.sh
@@ -1,6 +1,4 @@
-# git-twitter commands
-alias gtp='git commit --allow-empty' # post
-alias gts='git push origin main' # sync/send (push)
-alias gtr='git fetch' # refresh (fetch)
-alias gtl='git log --format="format:%Cred%cd %Cblue%h %Cgreen%cn%Creset: %s" --all --date=iso-local' # log
-alias gtv='git show' # view
+# git-twitter timeline
+alias gtl='git log --format="format:%Cred%cd %Cblue%h %Cgreen%cn%Creset: %s" --all --date=iso-local'
+# git commit empty
+alias gce='git commit --allow-empty'
