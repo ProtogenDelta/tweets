82c147618a4656dfd3d52e2b80e1fd20b781a759
Delta <47292121+ProtogenDelta@users.noreply.github.com>
2022-11-16 22:17:51 +1000

    removed files because bad
    So maybe it's not the best idea to clutter people's repos with pointless
    files... sorry!

--- files/aliases.sh
+++
@@ -1,4 +0,0 @@
-# git-twitter timeline
-alias gtl='git log --format="format:%Cred%cd %Cblue%h %Cgreen%cn%Creset: %s" --all --date=iso-local'
-# git commit empty
-alias gce='git commit --allow-empty'
